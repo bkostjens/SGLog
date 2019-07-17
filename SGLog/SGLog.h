338d82b2fd38c6a10158187ced8a459c02b0d67a
Barry Kostjens <b.kostjens@prosilic.nl>
2019-07-17 09:01:01 +0200

    Initial Commit

---
+++ SGLog/SGLog.h
@@ -0,0 +1,19 @@
+//
+//  SGLog.h
+//  SGLog
+//
+//  Created by Barry Kostjens on 17/07/2019.
+//  Copyright © 2019 Prosilic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGLog.
+FOUNDATION_EXPORT double SGLogVersionNumber;
+
+//! Project version string for SGLog.
+FOUNDATION_EXPORT const unsigned char SGLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGLog/PublicHeader.h>
+
+
